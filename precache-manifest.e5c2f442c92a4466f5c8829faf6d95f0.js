b8c30371cebc355de3c20cac205d09d7076a9d4a
FreakyTurtle <jagues@gmail.com>
2018-11-30 03:49:27 +0000

    Updates

---
+++ precache-manifest.e5c2f442c92a4466f5c8829faf6d95f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/animal-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9fb3502a842900bcf793",
+    "url": "/animal-list/static/js/runtime~main.9fb3502a.js"
+  },
+  {
+    "revision": "3753456188826bc78241",
+    "url": "/animal-list/static/js/main.37534561.chunk.js"
+  },
+  {
+    "revision": "909fa9192d7d57e1dbda",
+    "url": "/animal-list/static/js/1.909fa919.chunk.js"
+  },
+  {
+    "revision": "3753456188826bc78241",
+    "url": "/animal-list/static/css/main.7948b65e.chunk.css"
+  },
+  {
+    "revision": "4771cb232c1ea61333454705c5474fbf",
+    "url": "/animal-list/index.html"
+  }
+];
